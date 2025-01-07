d06ee71ec0277cc0d4489b3667766154138c25e0
giovinat <theo.giovinazzi@grenoble-inp.org>
2025-01-07 13:52:18 +0100

    test: ajout de test pour printl

---
+++ src/test/deca/context/valid/provided/printl-variable.java
@@ -0,0 +1,13 @@
+// Description:
+//    Programme minimaliste affichant une variable de type entier.
+//
+// Resultats:
+//    12
+//
+// Historique:
+//    cree le 07/01/2025
+
+{
+        int a = 12;
+        println(a);
+}
