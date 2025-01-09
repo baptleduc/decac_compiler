894e72a91db03c300caeff2c804308042b8c2bb1
Ryan_Lrd <ryan.elaroud@gmail.com>
2025-01-09 16:23:20 +0100

    feat: debut not, surement faux

--- src/main/java/fr/ensimag/deca/tree/Not.java
+++ src/main/java/fr/ensimag/deca/tree/Not.java
@@ -20,7 +20,7 @@ public class Not extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        return this.operand.verifyExpr();
     }
 
     @Override
