34957b9934df8df5897c41a6f52ce9c3b223b137
Baptiste Le Duc <baptiste.le-duc@grenoble-inp.org>
2025-01-09 17:14:39 +0100

    style: autoformat

--- src/main/java/fr/ensimag/deca/DecacCompiler.java
+++ src/main/java/fr/ensimag/deca/DecacCompiler.java
@@ -373,10 +373,9 @@ public class DecacCompiler {
         }
         prog.verifyProgram(this);
         assert (prog.checkAllDecorations());
-        if(getCompilerOptions().getVerify() == true){
+        if (getCompilerOptions().getVerify() == true) {
             return false;
         }
-
 
         addComment("start main program");
         prog.codeGenProgram(this);
