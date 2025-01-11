e2731d25bc7b81c4acf4d5175b312cf563b37278
Baptiste Le Duc <baptiste.le-duc@grenoble-inp.org>
2025-01-11 21:00:52 +0100

    feat: And bool implementation

--- src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpBool.java
@@ -1,10 +1,14 @@
 package fr.ensimag.deca.tree;
 
+import org.apache.log4j.Logger;
+
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.context.Type;
+import fr.ensimag.ima.pseudocode.DVal;
+import fr.ensimag.ima.pseudocode.GPRegister;
 
 /**
  *
@@ -12,11 +16,14 @@ import fr.ensimag.deca.context.Type;
  * @date 01/01/2025
  */
 public abstract class AbstractOpBool extends AbstractBinaryExpr {
+    private static final Logger LOG = Logger.getLogger(AbstractOpBool.class);
+
 
     public AbstractOpBool(AbstractExpr leftOperand, AbstractExpr rightOperand) {
         super(leftOperand, rightOperand);
     }
 
+    abstract protected void codeGenOpBool(GPRegister dest, DVal source, DecacCompiler compiler);
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
@@ -32,5 +39,6 @@ public abstract class AbstractOpBool extends AbstractBinaryExpr {
                         + "must be boolean values for logical operations (AND, OR)",
                 this.getRightOperand().getLocation());
     }
+
 
 }
--- src/main/java/fr/ensimag/deca/tree/And.java
+++ src/main/java/fr/ensimag/deca/tree/And.java
@@ -1,12 +1,27 @@
 package fr.ensimag.deca.tree;
 
+import org.apache.log4j.Logger;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.ima.pseudocode.DVal;
+import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.BNE;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+
 /**
  *
  * @author gl12
  * @date 01/01/2025
  */
 public class And extends AbstractOpBool {
+    private static final Logger LOG = Logger.getLogger(And.class);
 
+    
     public And(AbstractExpr leftOperand, AbstractExpr rightOperand) {
         super(leftOperand, rightOperand);
     }
@@ -19,6 +34,42 @@ public class And extends AbstractOpBool {
     @Override
     protected boolean isImmediate() {
         return false;
+    }
+
+    @Override
+    protected void codeGenOpBool(GPRegister dest, DVal source, DecacCompiler compiler){
+
+    }
+
+    @Override
+    protected void codeGenInst(DecacCompiler compiler){
+        getLeftOperand().codeGenInst(compiler);
+        getRightOperand().codeGenInst(compiler);
+
+        DVal leftDVal = getLeftOperand().getDVal(compiler);
+        DVal rightDVal = getRightOperand().getDVal(compiler);
+
+        GPRegister regLeft = leftDVal.codeGenToGPRegister(compiler);
+
+        Label setFalse = new Label("set_false");
+        Label end = new Label("end");
+        
+        
+        compiler.addInstruction(new CMP(new ImmediateInteger(0), regLeft));
+        compiler.addInstruction(new BEQ(setFalse));
+        GPRegister regRight = rightDVal.codeGenToGPRegister(compiler);
+        compiler.addInstruction(new CMP(0, regRight));
+        compiler.addInstruction(new BEQ(setFalse));
+        
+        compiler.addInstruction(new LOAD(new ImmediateInteger(1), regLeft));
+        compiler.addInstruction(new BRA(end));
+
+        compiler.addLabel(setFalse);
+        compiler.addInstruction(new LOAD(new ImmediateInteger(0), regLeft));
+
+        compiler.addLabel(end);
+        setDVal(regLeft);
+
     }
 
 }
