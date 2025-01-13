047d01c03b4afffd5df91efcabecffa4d3a912bc
Mmalo Nicolas <malo.nicolas-coqueron@grenoble-inp.org>
2025-01-13 15:06:29 +0100

    feat: nouveaux objets Selection et Return

---
+++ src/main/java/fr/ensimag/deca/tree/Return.java
@@ -0,0 +1,50 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author nicolmal
+ * @date 13/01/2025
+ */
+public class Return extends AbstractInst {
+    private AbstractExpr returnExpr;
+
+    public Return(AbstractExpr expr) {
+        this.returnExpr = expr;
+    }
+
+    @Override
+    protected void verifyInst(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass, Type returnType)
+            throws ContextualError {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+}
---
+++ src/main/java/fr/ensimag/deca/tree/Selection.java
@@ -0,0 +1,74 @@
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.Definition;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author nicolmal
+ * @date 13/01/2025
+ */
+public class Selection extends AbstractLValue {
+    @Override
+    protected void checkDecoration() {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    private AbstractExpr leftOperand;
+    private AbstractIdentifier rightOperand;
+
+    public Selection(AbstractExpr leftOperand, AbstractIdentifier rightOperand) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    private Definition definition;
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    String prettyPrintNode() {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void prettyPrintType(PrintStream s, String prefix) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    protected boolean isImmediate() {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+}
