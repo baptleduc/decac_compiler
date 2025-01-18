a62c33e853c048d905ae1a03aa81bf8f1a9742c1
Baptiste Le Duc <baptiste.le-duc@grenoble-inp.org>
2025-01-18 23:29:11 +0100

    feat: decompile for New

--- src/main/java/fr/ensimag/deca/tree/New.java
+++ src/main/java/fr/ensimag/deca/tree/New.java
@@ -47,7 +47,9 @@ public class New extends AbstractExpr {
 
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("not yet implemented");
+        s.print("new ");
+        ident.decompile(s);
+        s.print("()");
     }
 
     @Override
