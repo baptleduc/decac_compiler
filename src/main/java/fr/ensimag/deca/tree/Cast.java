cd7e406b3ae5e5f2411e488aafabf396fbc622ee
Baptiste Le Duc <baptiste.le-duc@grenoble-inp.org>
2025-01-18 16:40:45 +0100

    feat: codegen for explicit cast

--- src/main/java/fr/ensimag/deca/tree/Cast.java
+++ src/main/java/fr/ensimag/deca/tree/Cast.java
@@ -1,6 +1,7 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.codegen.LabelManager;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ClassType;
 import fr.ensimag.deca.context.ContextualError;
@@ -9,8 +10,14 @@ import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.tools.DecacInternalError;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.ima.pseudocode.DVal;
+import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.FLOAT;
+import fr.ensimag.ima.pseudocode.instructions.INT;
 import java.io.PrintStream;
+
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -18,6 +25,7 @@ import java.io.PrintStream;
  * @date 13/01/2025
  */
 public class Cast extends AbstractExpr {
+    private static final Logger LOG = Logger.getLogger(Cast.class);
 
     private AbstractIdentifier typeIdentifier;
     private AbstractExpr expressionToCast;
@@ -39,13 +47,13 @@ public class Cast extends AbstractExpr {
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
         Type typeExpression = expressionToCast.verifyExpr(compiler, localEnv, currentClass);
+        LOG.debug("typeExpression: " + typeExpression);
         Type typeCast = typeIdentifier.verifyType(compiler);
         if (typeExpression.isVoid()) {
             throw new ContextualError("Can't cast a void", expressionToCast.getLocation());
         }
         if ((typeExpression.isInt() && typeCast.isFloat())
                 || (typeExpression.isFloat() && typeCast.isInt())) {
-            expressionToCast.setType(typeCast);
             return typeCast;
         }
 
@@ -55,7 +63,6 @@ public class Cast extends AbstractExpr {
                 expressionToCast.getLocation());
         if ((classTypeExpression.isSubClassOf(classTypeCast))
                 || (classTypeCast.isSubClassOf(classTypeExpression))) {
-            expressionToCast.setType(classTypeCast);
             return classTypeCast;
         } else {
             throw new ContextualError("Can't cast those expressions", expressionToCast.getLocation());
@@ -87,13 +94,41 @@ public class Cast extends AbstractExpr {
     }
 
     @Override
-    protected DVal getDVal(DecacCompiler compiler) {
-        throw new UnsupportedOperationException("not yet implemented");
-    }
-
-    @Override
     protected void codeGenInst(DecacCompiler compiler) {
-        throw new UnsupportedOperationException("not yet implemented");
+        Type type = typeIdentifier.getType();
+        Type expressionType = expressionToCast.getType();
+
+        if ((type.isBoolean() || type.isInt() || type.isFloat()) && expressionType.sameType(type)) {
+            expressionToCast.codeGenInst(compiler);
+            setDVal(expressionToCast.getDVal(compiler));
+        
+        } else if (type.isFloat() && expressionType.isInt()) {
+            expressionToCast.codeGenInst(compiler);
+            DVal exprDVal = expressionToCast.getDVal(compiler);
+            GPRegister reg = exprDVal.codeGenToGPRegister(compiler);
+            compiler.addInstruction(new FLOAT(exprDVal, reg));
+            setDVal(reg);
+        } else if (type.isInt() && expressionType.isFloat()) {
+            expressionToCast.codeGenInst(compiler);
+            DVal exprDVal = expressionToCast.getDVal(compiler);
+            GPRegister reg = exprDVal.codeGenToGPRegister(compiler);
+            compiler.addInstruction(new INT(exprDVal, reg));
+            setDVal(reg);
+        } else {
+            InstanceOf instanceOf = new InstanceOf(expressionToCast, typeIdentifier);
+            Label elseCast = new Label("ElseCast");
+            Label endCast = new Label("EndCast");
+
+            instanceOf.codeGenBool(compiler, elseCast, false);
+            expressionToCast.codeGenInst(compiler);
+            compiler.addInstruction(new BRA(endCast));
+
+            compiler.addLabel(elseCast);
+            compiler.addInstruction(new BRA(LabelManager.CAST_ERROR.getLabel()));
+
+            compiler.addLabel(endCast);
+            setDVal(expressionToCast.getDVal(compiler));
+        }
     }
 
     @Override
